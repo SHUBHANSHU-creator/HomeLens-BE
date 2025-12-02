18d4a46ebd202797730fb245529f53829801b5f0
Shubhanshu Kumar <shukumarbham0@gmail.com>
2025-12-02 21:46:33 +0530

    Project Setup

---
+++ HomeLens_backend/src/main/java/com/HomeLens_backend/api/HomeLensBackendApplication.java
@@ -0,0 +1,14 @@
+package com.HomeLens_backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLensBackendApplication {
+
+	public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(HomeLensBackendApplication.class, args);
+	}
+
+}
---
+++ HomeLens_backend/src/test/java/com/HomeLens_backend/api/HomeLensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeLens_backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
